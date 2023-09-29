638d3d995ff129b3aa1a7c5c05d084dc75b512e1
raahulvasili <146322149+raahulvasili@users.noreply.github.com>
2023-09-29 12:13:42 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone!'
+    return 'Hello, everyone! This is Raahul and my Jenkins is successfull'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
