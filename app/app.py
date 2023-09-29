0f1c93338c93baea69e2bfb18fedeb68e611f315
raahulvasili <146322149+raahulvasili@users.noreply.github.com>
2023-09-29 12:32:29 +0530

    Update app.py
    message

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, everyone! This is Raahul Vasili'
+    return 'Hello, everyone! This is Vasili'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
